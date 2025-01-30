10fffbdd657b53a4384fe9aac9f4e760b6f83445
YAKAN3105 <shanyexiongqing@gmail.com>
2025-01-30 18:45:25 +0900

    up

--- ArrowShooting/Player.cpp
+++ ArrowShooting/Player.cpp
@@ -4,10 +4,13 @@
 Player::Player():
 	i(0),
 	m_handle(0),
-	m_AnimHandle(0),
-	m_RunHandle(0),
-	m_IdleHandle(0),
-	maxAnimNum(0)
+	m_animHandle(0),
+	m_runHandle(0),
+	m_idleHandle(0),
+	m_jumpHandle(-1),
+	maxAnimNum(0),
+	m_isDirLeft(false),
+	m_isJump(false)
 {
 	m_handle = 0;
 }
@@ -18,9 +21,10 @@ Player::~Player()
 
 void Player::Init()
 {
-	m_IdleHandle = LoadGraph("data/image1/Sprites/King/Idle.png");
-	m_RunHandle = LoadGraph("data/image1/Sprites/King/Run.png");
-	m_handle = m_IdleHandle;
+	m_idleHandle = LoadGraph("data/image1/Sprites/King/Idle.png");
+	m_runHandle = LoadGraph("data/image1/Sprites/King/Run.png");
+	m_jumpHandle = LoadGraph("data/image1/Sprites/King/Jump.png");
+	m_handle = m_idleHandle;
 	i = 0;
 	maxAnimNum = 10;
 	m_pos = Vec2(500, 500);//初期位置
@@ -36,7 +40,7 @@ void Player::Update()
 void Player::Draw()
 {
 	//DrawGraph(0, 0, m_handle, TRUE);
-	DrawRectGraph(m_pos.x, m_pos.y, 78*i, 0, 78, 58, m_handle, TRUE, FALSE);
+	DrawRectGraph(m_pos.x, m_pos.y, 78*i, 0, 78, 58, m_handle, TRUE, m_isDirLeft);
 }
 
 void Player::AnimRoop()
@@ -54,7 +58,7 @@ void Player::AnimRoop()
 	}
 
 	//アニメーションのループリセット
-	if (i == maxAnimNum)
+	if (i >= maxAnimNum)
 	{
 		i = 0;
 	}
@@ -62,23 +66,31 @@ void Player::AnimRoop()
 
 void Player::Move()
 {
-
-	if (CheckHitKey(KEY_INPUT_RIGHT) && m_pos.x < 1280 -80)
+	if (CheckHitKey(KEY_INPUT_UP))
 	{
+		m_isJump = TRUE;
+		m_pos.y -= 10;
+		m_handle = m_jumpHandle;
+		maxAnimNum = 0;
+	}
+	else if (CheckHitKey(KEY_INPUT_RIGHT) && m_pos.x < 1280 -80)
+	{
+		m_isDirLeft = FALSE;
 		m_pos.x += 4;
-		m_handle = m_RunHandle;
+		m_handle = m_runHandle;
 		maxAnimNum=7;
 	}
 	else if (CheckHitKey(KEY_INPUT_LEFT) && m_pos.x > 20)
 	{
+		m_isDirLeft = TRUE;
 		m_pos.x -= 4;
-
+		m_handle = m_runHandle;
+		maxAnimNum = 7;
 	}
 	else
 	{
-		m_handle = m_IdleHandle;
+		m_handle = m_idleHandle;
 	}
-
 }
 
 void Player::Gravity()
--- ArrowShooting/Player.h
+++ ArrowShooting/Player.h
@@ -13,12 +13,18 @@ public:
 	void Gravity();
 private:
 	int m_handle;
-	int	m_AnimHandle;
-	int m_RunHandle;
-	int m_IdleHandle;
+	int	m_animHandle;
+	int m_runHandle;
+	int m_idleHandle;
+	int m_jumpHandle;
+	int m_speed = 0;
+	int m_jumpSpeed = 0;
 	int i = 0;
 	int maxAnimNum;
 	int	AnimInterval = 0;
+	bool m_isDirLeft;
+	bool m_isJump;
 	Vec2 m_pos;
+	Vec2 m_velocity;
 };
 
